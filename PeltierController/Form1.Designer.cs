c124bb1f44b83617b2c63cedfbdf67575de040ef
braml <bramlaurens@gmail.com>
2025-02-07 09:21:47 +0100

    Set up serial framework

--- PeltierController/Form1.Designer.cs
+++ PeltierController/Form1.Designer.cs
@@ -31,6 +31,11 @@
             label1 = new Label();
             label2 = new Label();
             button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -47,7 +52,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(290, 20);
+            label2.Location = new Point(419, 20);
             label2.Name = "label2";
             label2.Size = new Size(115, 48);
             label2.TabIndex = 1;
@@ -55,7 +60,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(21, 87);
+            button1.Location = new Point(21, 185);
             button1.Name = "button1";
             button1.Size = new Size(164, 43);
             button1.TabIndex = 2;
@@ -63,16 +68,69 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(21, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(298, 48);
+            label3.TabIndex = 3;
+            label3.Text = "Controller value:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(419, 68);
+            label4.Name = "label4";
+            label4.Size = new Size(115, 48);
+            label4.TabIndex = 4;
+            label4.Text = "label4";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(1356, 765);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1259, 766);
+            button2.Name = "button2";
+            button2.Size = new Size(81, 34);
+            button2.TabIndex = 6;
+            button2.Text = "Refresh";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1099, 766);
+            button3.Name = "button3";
+            button3.Size = new Size(154, 34);
+            button3.TabIndex = 7;
+            button3.Text = "Connect";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1550, 810);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "PeltierController";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -82,5 +140,10 @@
         private Label label1;
         private Label label2;
         private Button button1;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button button3;
     }
 }
--- PeltierController/Form1.cs
+++ PeltierController/Form1.cs
@@ -1,8 +1,27 @@
+using System.IO;
+using System.IO.Ports;
+
+
+//Peltier controller GUI
+//Author: Bram Laurens
+//February 2025
+//Dit programma heeft als doel het aansturen van een peltier module.
+//De module is aangesloten op een Roboteq Motor Controller.
+//De controller genereert PWM signalen met een duty cycle in promille op basis van
+//seriele commando's in ASCII formaat
+
+
 namespace PeltierController
 {
     public partial class Form1 : Form
     {
+        //Class variables
         static bool coolingStatus;
+        static int peltierOutput;
+
+        //Make a new serialport object
+        private SerialPort Serial = new SerialPort();
+
         public Form1()
         {
             InitializeComponent();
@@ -10,14 +29,86 @@ namespace PeltierController
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        //Function to update com port's list when called
+        private void UpdateCOMportList()
         {
-            
+            //Refresh the COM ports, put the ports in an array and clear the comboBox. 
+            String[] Ports = System.IO.Ports.SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+
+            //Add all ports from the array to the comboBox
+            foreach (var item in Ports)
+            {
+                comboBox1.Items.Add(item);
+            }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Call the update COM ports function upon loading the window
+            UpdateCOMportList();
+        }
+
+        //Reset peltier button click event
         private void button1_Click(object sender, EventArgs e)
         {
             coolingStatus = true;
+            peltierOutput = 0;
+            label2.Text = "Peltier Disabled";
+            label4.Text = peltierOutput.ToString();
+        }
+
+        //Refresh COM ports button click event
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateCOMportList();
+        }
+
+        //Connect COM button click event
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Set the Serial port object according to selected comboBox item
+            try
+            {
+                Serial.PortName = comboBox1.Text;
+            }
+            catch 
+            {
+                MessageBox.Show("No COM port selected!");
+            }
+
+            //Set fixed Serial port object arguments 
+            Serial.BaudRate = 9600;
+            Serial.DataBits = 8;
+            Serial.ReceivedBytesThreshold = 1;
+            Serial.StopBits = StopBits.One;
+            Serial.Handshake = Handshake.None;
+            Serial.WriteTimeout = 3000;
+
+            //If the serial port is not open yet
+            if (false == Serial.IsOpen) 
+            {
+                //Open the serial port
+                try 
+                {
+                    Serial.Open();
+
+                }
+                catch
+                {
+                    MessageBox.Show("Error: The COM port is not accesible. Close all applications using this port.");
+                    return;
+                }
+            }
+
+            //If Serial port is open, change button text to disconnect, and disable the combobox & COM ports button
+            if(true == Serial.IsOpen)
+            {
+                button3.Text = "Disconnect";
+                comboBox1.Enabled = false;
+                button2.Enabled = false;
+            }
+
         }
     }
 }
